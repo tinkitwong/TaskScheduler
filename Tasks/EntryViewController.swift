f2f376ba98dc0f5fcbd8aafcc5b83b8ca4482a86
tinkitwong <tinkitwong@gmail.com>
2020-04-30 12:13:03 +0800

    Added Delete and updated Adding Functions

--- Tasks/EntryViewController.swift
+++ Tasks/EntryViewController.swift
@@ -14,9 +14,12 @@ class EntryViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var field: UITextField!
     
+    var update: (() -> Void)?
     override func viewDidLoad() {
         super.viewDidLoad()
-        field.delegate = self 
+        field.delegate = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveTasks))
         // Do any additional setup after loading the view.
     }
     
@@ -25,5 +28,21 @@ class EntryViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
-    @IBAction func saveTasks() {}
+    @objc func saveTasks() {
+        // guard against null values
+        guard let text = field.text, !text.isEmpty else {
+            return
+        }
+        
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+        
+        let newCount = count + 1
+        UserDefaults().set(newCount, forKey: "count")
+        UserDefaults().set(text, forKey: "task_\(newCount)")
+        
+        update?()
+        navigationController?.popViewController(animated: true)
+    }
 }
